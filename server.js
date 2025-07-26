947d99e0bf0b2f3edf2cb8c7f4b5a512a8914743
LukinhasTK <lucas.tkn14@gmail.com>
2025-07-26 10:24:57 -0300

    Código marcando cada tipo de rota

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const app = express()
 
 app.use(express.json())
 
+// Rota para achar o usuário criado
 app.get('/usuarios', async (req, res) => {
   const users = await prisma.user.findMany()
   res.status(200).json(users)
 })
 
+// Rota para criar usuários
 app.post('/usuarios', async (req, res) => {
   const user = await prisma.user.create({
     data: {
@@ -22,6 +24,8 @@ app.post('/usuarios', async (req, res) => {
   res.status(201).json(user)
 })
 
+
+// Rota para editar o usuário
 app.put('/usuarios/:id', async (req, res) => {
   const user = await prisma.user.update({
     where: { id: req.params.id },
@@ -34,6 +38,7 @@ app.put('/usuarios/:id', async (req, res) => {
   res.status(200).json(user)
 })
 
+// Rota para deletar o usuário
 app.delete('/usuarios/:id', async (req, res) => {
   const user = await prisma.user.delete({
     where: { id: req.params.id }
@@ -41,6 +46,7 @@ app.delete('/usuarios/:id', async (req, res) => {
   res.status(200).json({ message: 'Usuário deletado com sucesso', user })
 })
 
+// Código para iniciar o servidor local
 app.listen(3000, () => {
   console.log('Server iniciado na porta 3000')
 })
